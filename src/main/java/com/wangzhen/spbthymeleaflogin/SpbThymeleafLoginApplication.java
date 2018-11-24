100e652c60dc24b174fb4f5003254603bb067fe7
wang.zhen <vq.momomiao>
2018-11-24 13:48:29 +0800

    提交代码

---
+++ src/main/java/com/wangzhen/spbthymeleaflogin/SpbThymeleafLoginApplication.java
@@ -0,0 +1,12 @@
+package com.wangzhen.spbthymeleaflogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbThymeleafLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbThymeleafLoginApplication.class, args);
+	}
+}
---
+++ src/main/java/com/wangzhen/spbthymeleaflogin/miao/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.wangzhen.spbthymeleaflogin.miao.controller;
+
+/**
+ * @ClassName MainController
+ * @Description
+ * @Author wangzhen
+ * @Date 2018/11/24 下午1:38
+ **/
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value="/main")
+    public String main(){
+        System.out.println("MainController main方法被调用......");
+        // 根据Thymeleaf默认模板，将返回resources/templates/main.html
+        return "main";
+    }
+
+}
